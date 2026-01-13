5f60ffd35bd9ab72d7a7347bcea14e68f65c8f2c
Yousra Azaim <azmyusra@gmail.com>
2026-01-13 19:51:43 +0100

    Updated App.java with new message

--- src/main/java/com/yousra/App.java
+++ src/main/java/com/yousra/App.java
@@ -3,5 +3,6 @@ package com.yousra;
 public class App {
     public static void main(String[] args) {
         System.out.println("Bonjour et bon courage dans votre projet en DevOps");
+        System.out.println("Modification from dev branch");
     }
 }
